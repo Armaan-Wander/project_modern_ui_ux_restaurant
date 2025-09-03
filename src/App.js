672e305c885421379248f9b3a89c40a61e6761cd
Armaan-Wander <115729086+Armaan-Wander@users.noreply.github.com>
2025-09-03 15:13:23 -0400

    started the cart
    idk what the issue is rn damn

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
 import React from 'react';
 // import { AboutUs, Chef, Footer, Gallery, Header, Intro, Laurels, SpecialMenu } from './container';
+import { BrowserRouter as Router } from 'react-router-dom';
 import { Footer, Gallery, Header, Intro, SpecialMenu } from './container';
 import { Navbar } from './components';
 import './App.css';
-
+// , Route, Link, Routes
 const App = () => (
-  <div>
-    <Navbar />
-    <Header />
-    {/* <AboutUs /> */}
-    <SpecialMenu />
-    <Gallery />
-    {/* <Chef /> */}
-    <Intro />
-    {/* <Laurels /> */}
-    <Footer />
-  </div>
+
+  <Router>
+    <div>
+      <Navbar />
+      <Header />
+      {/* <AboutUs /> */}
+      <SpecialMenu />
+      <Gallery />
+      {/* <Chef /> */}
+      <Intro />
+      {/* <Laurels /> */}
+      <Footer />
+    </div>
+  </Router>
 );
 
 export default App;
